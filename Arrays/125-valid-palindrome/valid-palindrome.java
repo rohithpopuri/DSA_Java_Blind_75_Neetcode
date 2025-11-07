cd0a82bca589708cb3c7472f1bab02256c06de2a
rohithpopuri <138302534+rohithpopuri@users.noreply.github.com>
2025-11-08 00:20:31 +0530

    Time: 2 ms (98.59%) | Memory: 44.2 MB (47.07%) - LeetSync

--- Arrays/125-valid-palindrome/valid-palindrome.java
+++ Arrays/125-valid-palindrome/valid-palindrome.java
@@ -1,28 +1,51 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String d=s.replaceAll("[^a-zA-Z0-9]", "");
-        d=d.toLowerCase();
-        // StringBuilder str = new StringBuilder(d);
-        // String f=str.reverse().toString();
-        // if (d.equalsIgnoreCase(f)){
-        //     return true ;
-        // }
-        // return false;
-        int left =0;
-        int right = d.length()-1 ;
+        // String d=s.replaceAll("[^a-zA-Z0-9]", "");
+        // d=d.toLowerCase();
+        // // StringBuilder str = new StringBuilder(d);
+        // // String f=str.reverse().toString();
+        // // if (d.equalsIgnoreCase(f)){
+        // //     return true ;
+        // // }
+        // // return false;
+        // int left =0;
+        // int right = d.length()-1 ;
 
-        while (left<right){
-            if (d.charAt(left)!=d.charAt(right)){
-                System.out.println(d.charAt(left));
+        // while (left<right){
+        //     if (d.charAt(left)!=d.charAt(right)){
+        //         System.out.println(d.charAt(left));
+        //         return false;
+        //     }
+        //     left++;
+        //     right--;
+
+
+
+        // }
+        // return true;
+        
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !alphaNum(s.charAt(l))) {
+                l++;
+            }
+            while (r > l && !alphaNum(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
                 return false;
             }
-            left++;
-            right--;
-
-
-
+            l++; r--;
         }
         return true;
 
+
+
+    }
+    public boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
     }
 }
