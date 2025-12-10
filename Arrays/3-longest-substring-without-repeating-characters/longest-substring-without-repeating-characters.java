cbd40f223242a22db63f0f9d20da90c538294c1a
rohithpopuri <138302534+rohithpopuri@users.noreply.github.com>
2025-12-10 12:37:07 +0530

    Time: 6 ms (67.88%) | Memory: 46.4 MB (21.37%) - LeetSync

--- Arrays/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ Arrays/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,24 +1,45 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
-        int size= s.length();
-        char[] arr = s.toCharArray();
-        int max_length =0 ;
-        for(int i =0 ; i< size;i++){
-            String S= arr[i]+"" ;
-            for(int j =i+1 ;j<size;j++){
-                if (S.contains(arr[j]+"")){
-                    break ;
-                }
-                S=S+arr[j];
-            }
-            System.out.println(S);
-            if (S.length()>max_length){
-                 max_length=S.length();
-            }
-            S="";
+        // my solution -462
+        // int size= s.length();
+        // char[] arr = s.toCharArray();
+        // int max_length =0 ;
+        // for(int i =0 ; i< size;i++){
+        //     String S= arr[i]+"" ;
+        //     for(int j =i+1 ;j<size;j++){
+        //         if (S.contains(arr[j]+"")){
+        //             break ;
+        //         }
+        //         S=S+arr[j];
+        //     }
+        //     System.out.println(S);
+        //     if (S.length()>max_length){
+        //          max_length=S.length();
+        //     }
+        //     S="";
            
+        // }
+        // return max_length ;
+
+
+        int left = 0;
+        int maxLen = 0;
+        HashSet<Character> set = new HashSet<>();
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            // If duplicate found, shrink window
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(c);
+            maxLen = Math.max(maxLen, right - left + 1);
         }
-        return max_length ;
+    
+        return maxLen;
     }
+    
 }
